98bc164a4b61ba64c6c911c2f5df3e22c2d41f5f
Kristina <kitty_kris@bk.ru>
2022-01-14 11:38:30 +0300

    add initializing of 2 users in controller

--- src/main/java/springboot/web/controllers/MainController.java
+++ src/main/java/springboot/web/controllers/MainController.java
@@ -1,10 +1,24 @@
 package springboot.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import springboot.web.service.RoleService;
+import springboot.web.service.UserService;
 
 @Controller
 public class MainController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public MainController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+        this.roleService.addDefaultRoles();
+        this.userService.addDefaultUsers();
+    }
 
     @GetMapping("/")
     public String home() {
