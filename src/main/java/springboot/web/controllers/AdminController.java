df6a41405a8f56a634617189ba2b0fb7799a10c1
Kristina <kitty_kris@bk.ru>
2022-01-25 21:24:15 +0300

    add users roles for filling table

--- src/main/java/springboot/web/controllers/AdminController.java
+++ src/main/java/springboot/web/controllers/AdminController.java
@@ -1,10 +1,12 @@
 package springboot.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import springboot.web.model.User;
+import springboot.web.service.RoleService;
 
 import java.security.Principal;
 
@@ -12,10 +14,18 @@ import java.security.Principal;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final RoleService roleService;
+
+    @Autowired
+    public AdminController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @GetMapping
     public String adminDetails(Model model, Principal principal) {
         User loginedUser = (User) ((Authentication) principal).getPrincipal();
         model.addAttribute("adminDetails", loginedUser);
+        model.addAttribute("allRoles", roleService.allRoles());
         return "adminDetails";
     }
 }
